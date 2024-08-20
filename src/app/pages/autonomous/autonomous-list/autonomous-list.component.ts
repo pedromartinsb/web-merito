459fde15207c47d1ecebce0810fe1ae0940f3846
Pedro Martins Barbosa Campos <engpedrocampos@gmail.com>
2024-08-20 16:15:47 -0300

    fix: adjust autonomous table

--- src/app/pages/autonomous/autonomous-list/autonomous-list.component.ts
+++ src/app/pages/autonomous/autonomous-list/autonomous-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -14,25 +15,22 @@ import { PersonService } from 'src/app/services/person.service';
   templateUrl: './autonomous-list.component.html',
   styleUrls: ['./autonomous-list.component.css'],
 })
-export class AutonomousListComponent implements OnInit {
-  persons: Person[] = [];
-
-  ELEMENT_DATA: Person[] = [];
-  FILTERED_DATA: Person[] = [];
-
+export class AutonomousListComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = [
+    'picture',
     'name',
-    'email',
-    'cpfCnpj',
-    'responsibility',
     'fantasyName',
+    'responsibility',
+    'routines',
+    'goals',
     'actions',
   ];
-  dataSource = new MatTableDataSource<Person>(this.persons);
+  dataSource = new MatTableDataSource<Person>();
 
   public isLoading: boolean = false;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private personService: PersonService,
@@ -48,11 +46,15 @@ export class AutonomousListComponent implements OnInit {
     this.findAll();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   findAll() {
     this.personService
       .findAllByContractType('Professional')
       .subscribe((response) => {
-        this.persons = response;
         this.dataSource = new MatTableDataSource<Person>(response);
         this.dataSource.paginator = this.paginator;
         this.isLoading = false;
@@ -62,6 +64,10 @@ export class AutonomousListComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   editPerson(personId: string): void {
