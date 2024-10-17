33cd52a3faa92674a97c99e1de94e0ab0168b9ff
Pedro Martins Barbosa Campos <engpedrocampos@gmail.com>
2024-10-17 15:07:26 -0300

    adjust on api config baseurl

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -8,12 +8,12 @@ import { environment } from "src/environments/environment";
 export class Config {
   public static get webApiUrl() {
     if (environment.production) {
-      // return 'https://api-merito-66f71383f3bc.herokuapp.com/api';
-      return 'https://201.23.18.233:8080/api';
+      return 'https://api-merito-66f71383f3bc.herokuapp.com/api';
+      // return 'https://201.23.18.233:8080/api';
     }
     if (environment.staging) {
-      // return 'https://api-merito-66f71383f3bc.herokuapp.com/api';
-      return 'https://201.23.18.233:8080/api';
+      return 'https://api-merito-66f71383f3bc.herokuapp.com/api';
+      // return 'https://201.23.18.233:8080/api';
     }
     return 'http://localhost:8080/api'
   }
